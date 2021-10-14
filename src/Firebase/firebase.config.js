8c7a0c18f139a6c09bb57e1186569174aa364462
hasibul1610 <bcse.hasibul@gmail.com>
2021-10-14 09:08:39 +0600

    added

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyC7InI75_qILmm1nAS-tcqfeFu8iAoY6V8",
+    authDomain: "ema-john-simple-38e9d.firebaseapp.com",
+    projectId: "ema-john-simple-38e9d",
+    storageBucket: "ema-john-simple-38e9d.appspot.com",
+    messagingSenderId: "635469270742",
+    appId: "1:635469270742:web:1283f5c3ba0adc19110377"
+};
+
+export default firebaseConfig;
